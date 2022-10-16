4964f75e260eaadad2bb8f0b5abca53caf5dc66e
kevlar-sudo <81199331+Kevlar-sudo@users.noreply.github.com>
2022-10-16 14:30:07 -0400

    Getting list of parts

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res)=>{
-    res.send('Hello World!');
-});
+//Parts store
+const parts =[
+    {id: 100,name:'Belt',colour:'brown'},
+    {id: 101,name:'Clip',colour:'brown'},
+    {id: 102,name:'Belt',colour:'red'},
+    {id: 103,name:'Hat',colour:'purple'},
+];
+
+//get list of parts
+app.get('/api/parts',(req,res)=>{
+    console.log(`GET request for ${req.url}`);
+    res.send(parts);
+})
 
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`);
